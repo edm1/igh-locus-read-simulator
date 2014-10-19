00bf57ec78ae511ac674b30be2a5c9fcca3e8584
Edward Mountjoy <ed@Edwards-MBP.lan>
2014-10-19 20:01:01 +0100

    adapted installer for mac os x

--- install_ART.sh
+++ install_ART.sh
@@ -1,4 +1,19 @@
+#!/bin/bash
+
+# Detect OS and correct link
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform
+    filen=http://www.niehs.nih.gov/research/resources/assets/docs/artbinvanillaicecream031114macos64tgz.tgz
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under Linux platform
+    filen=http://www.niehs.nih.gov/research/resources/assets/docs/artbinvanillaicecream031114linux64tgz.tgz
+fi
+
+# Make and change to lib folder
 mkdir lib
 cd lib
-wget http://www.niehs.nih.gov/research/resources/assets/docs/artbinvanillaicecream031114linux64tgz.tgz
-tar zxvf artbinvanillaicecream031114linux64tgz.tgz
+
+# Get file and uncompress
+curl -O $filen
+fileb=${filen##*/}
+tar zxvf $fileb
