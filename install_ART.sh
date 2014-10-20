5214491b472ad64df7761f08697a14ca6ff8c253
Ed Mountjoy <emountjoy@gmail.com>
2014-10-20 09:58:04 +0100

    curl not installed on linux

--- install_ART.sh
+++ install_ART.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-# Detect OS and correct link
-if [ "$(uname)" == "Darwin" ]; then
-    # Do something under Mac OS X platform
-    filen=http://www.niehs.nih.gov/research/resources/assets/docs/artbinvanillaicecream031114macos64tgz.tgz
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    # Do something under Linux platform
-    filen=http://www.niehs.nih.gov/research/resources/assets/docs/artbinvanillaicecream031114linux64tgz.tgz
-fi
-
 # Make and change to lib folder
 mkdir lib
 cd lib
 
-# Get file and uncompress
-curl -O $filen
+# Detect OS, correct link and get file
+if [ "$(uname)" == "Darwin" ]; then
+    filen=http://www.niehs.nih.gov/research/resources/assets/docs/artbinvanillaicecream031114macos64tgz.tgz
+    curl -O $filen
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    filen=http://www.niehs.nih.gov/research/resources/assets/docs/artbinvanillaicecream031114linux64tgz.tgz
+    wget $filen
+fi
+
+# Uncompress
 fileb=${filen##*/}
 tar zxvf $fileb
